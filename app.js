e733c6b65751d6d92ed12cf7c84fadfeba55e480
MattFree <matthewfreeland@outlook.com>
2021-02-25 18:01:12 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const tmi = require('tmi.js')
+var prefix = "!"
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.BOT_USERNAME,
+		password: process.env.OAUTH_TOKEN
+	},
+	channels: [ process.env.CHANNEL_NAME ]
+});
+
+client.connect().catch(console.error);
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+
+	//cmd handler
+	const args = message.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	try {
+		let commandFile = require(`./commands/${cmd}.js`)
+		commandFile.run(client, message, args, user, channel, self)
+	} catch (err) {
+		//client.say(channel, "Command doesn't exist.")
+		return;
+	}
+});
